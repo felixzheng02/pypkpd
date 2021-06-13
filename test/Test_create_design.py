0f6b1882b799805837af032f5263e9b4590ecc77
felixzheng02 <felix02@email.unc.edu>
2021-06-13 13:40:53 +0800

    Changed test

--- test/Test_create_design.py
+++ test/Test_create_design.py
@@ -2,7 +2,7 @@ import project.create_design
 import numpy as np
 
 
-xt1 = [np.array([1, 2, 3]), np.array([1, 2, 3, 4])]
+xt1 = [np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 3.0, 4.0])]
 
 
 design_1 = project.create_design.create_design(xt=xt1, groupsize=20)

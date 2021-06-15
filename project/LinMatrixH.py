045d8de16718d45d96ea646386f16b3a13368f12
Caiya-Zhang <69797470+Caiya-Zhang@users.noreply.github.com>
2021-06-15 20:44:59 +0800

    mainly for ind_estimates

---
+++ project/LinMatrixH.py
@@ -0,0 +1,94 @@
+"""
+#' Model linearization with respect to epsilon.
+#' 
+#' The function performs a linearization of the model with respect to the residual variability.
+#' Derivative of model w.r.t. eps evaluated at eps=0
+#' 
+
+"""
+
+
+import zeros
+import feval
+import grad_all
+import numpy as np
+
+#' @inheritParams mftot
+#' @inheritParams RS_opt
+#' @inheritParams evaluate.fim
+#' @inheritParams Doptim
+#' @inheritParams create.poped.database
+#' @param xt_ind A vector of the individual/group sample times
+#' @param b_ind vector of individual realization of the BSV terms b
+#' @param bocc_ind Vector of individual realizations of the BOV terms bocc
+#' 
+#' @return A matrix of size (samples per individual x number of epsilons) 
+#'  
+#' @family FIM
+#'     
+# @example tests/testthat/examples_fcn_doc/warfarin_optimize.R
+# @example tests/testthat/examples_fcn_doc/examples_LinMatrixH.R
+# @export
+#' @keywords internal
+## Function translated automatically using 'matlab.to.r()'
+## Author: Andrew Hooker
+
+def LinMatrixH (model_switch,xt_ind,x,a,bpop,b_ind,bocc_ind,poped_db):
+  #----------Model linearization with respect to epsilon.
+  #
+  # size of return is (samples per individual x number of epsilons) 
+  #
+  # derivative of model w$r.t. eps eval at e=0
+  #
+    NumEPS = poped_db["parameters"]["sigma"].shape[0]
+    if NumEPS==0:
+        y=0
+    else:
+        returnArgs = gradf_eps(model_switch,xt_ind,x,a,bpop,b_ind,bocc_ind,NumEPS,poped_db) 
+        y = returnArgs[[0]]
+        poped_db = returnArgs[[1]]
+    return [y, poped_db]
+
+
+#' Model linearization with respect to epsilon.
+#' 
+#' The function performs a linearization of the model with respect to the residual variability.
+#' Derivative of model w.r.t. eps evaluated at eps=0 and b=b_ind.
+#' 
+#' @inheritParams mftot
+#' @inheritParams Doptim
+#' @inheritParams create.poped.database
+#' @inheritParams LinMatrixH
+#' @param num_eps The number of \code{eps()} in the model.
+#' 
+#' @return A matrix of size (samples per individual x number of epsilons) 
+#'  
+#' @family FIM
+#' @example tests/testthat/examples_fcn_doc/warfarin_optimize.R
+#' @example tests/testthat/examples_fcn_doc/examples_gradf_eps.R
+#' @export
+#' @keywords internal
+#' 
+
+
+def gradf_eps (model_switch,xt_ind,x,a,bpop,b_ind,bocc_ind,num_eps,poped_db):
+  #----------Model linearization with respect to epsilon.
+  #
+  # size of return is (samples per individual x number of epsilons)
+  #
+  # derivative of model w$r.t. eps eval at e=0 and b=b_ind
+  #
+  #
+  
+    if poped_db["settings"]["iApproximationMethod"]==0 or poped_db["settings"]["iApproximationMethod"]==1:#No interaction
+        fg0=feval(poped_db["model"]["fg_pointer"],x,a,bpop,zeros(b_ind.shape[0],b_ind.shape[1]), zeros(bocc_ind.shape[0],bocc_ind.shape[1]))
+    
+    else:
+        fg0=feval(poped_db["model"]["fg_pointer"],x,a,bpop,b_ind,bocc_ind) #Interaction
+  
+    e0=zeros(1,num_eps)
+    dfeps_de0 = grad_all(poped_db["model"]["ferror_pointer"],4,xt_ind.shape[0],model_switch,xt_ind,fg0,e0,poped_db)
+  
+    return [dfeps_de0, poped_db]
+
+
--- project/ind_estimates.py
+++ project/ind_estimates.py
@@ -5,20 +5,24 @@ AUTHOR: Caiya Zhang, Yuchen Zheng
 """
 
 
+import numpy as np
+import diag_matlab
+
+
 #Get the emperical bayes estimates for one individual
 #Written for PopED by JN
-def ind_estimates (data,bpop,d,sigma,start_bind,bInter,bUDDLike,model_switch,xt_ind,x,a,b_ind,bocc_ind,poped_db):
+def ind_estimates (data,bpop,d,sigma,start_bind:np.ndarray,bInter,bUDDLike,model_switch,xt_ind,x,a,b_ind,bocc_ind,poped_db):
   
-    b_i = t(start_bind)
-    c1 = length(t(start_bind))/2*log(2*pi)
-    c2 = 1/2*log(det(d))
-    c3 = inv(d)
+    b_i = np.transpose(start_bind)
+    c1 = ((np.transpose(start_bind))/2*np.log(2*np.pi)).size
+    c2 = 1/2*np.log(np.linalg.det(d))
+    c3 = np.linalg.inv(d)
   
-    if (bInter == False and bUDDLike == False :#Calculate only one variance for all samples
+    if bInter == False and bUDDLike == False :#Calculate only one variance for all samples
         #eps = zeros(size(tdata,1),size(sigma,2))
-        h = LinMatrixH(model_switch,xt_ind,x,a,bpop,b_ind,bocc_ind,poped.db)
-        res_var = diag_matlab(diag_matlab(h%*%sigma%*%t(h)))
-        lC=solve(chol(res_var),diag_matlab(length(res_var))) #Calculate cholesky factorization
+        h = LinMatrixH(model_switch,xt_ind,x,a,bpop,b_ind,bocc_ind,poped_db)
+        res_var = diag_matlab(diag_matlab(np.matmul(np.matmul(h,sigma),h)))
+        lC = solve(chol(res_var),diag_matlab(length(res_var))) #Calculate cholesky factorization
         det_res_var = det(res_var)
     else
         lC = 0
@@ -43,17 +47,16 @@ def min_function (data,bpop,d,sigma,bInter,bUDDLike,model_switch,xt_ind,x,a,bocc
     bAnalytic = False
     li = ind_likelihood(data,bpop,d,sigma,bInter,bUDDLike,model_switch,xt_ind,x,a,bocc_ind,poped.db,lC,det_res_var,b_ind) #Individual log likelihood
     ret =c1+c2+1/2*t(b_ind)*c3*b_ind-li
-    if (return_deriv)):
-        if (!bAnalytic)):
+    if return_deriv:
+        if !bAnalytic:
             dret=zeros(length(d),1)
             heta=1e-8
-        for i in 1:len(d):
-            bind_plus=b_ind
-            bind_plus[i]=b_ind(i)+heta
-            li_plus = ind_likelihood(data,bpop,d,sigma,bInter,bUDDLike,model_switch,xt_ind,x,a,bocc_ind,poped.db,lC,det_res_var,bind_plus) #Individual log likelihood
-            dret[i]=(li_plus-li)/heta
+            for i in 1:len(d):
+                bind_plus=b_ind
+                bind_plus[i]=b_ind(i)+heta
+                li_plus = ind_likelihood(data,bpop,d,sigma,bInter,bUDDLike,model_switch,xt_ind,x,a,bocc_ind,poped.db,lC,det_res_var,bind_plus) #Individual log likelihood
+                dret[i]=(li_plus-li)/heta
         #}# else {
         #  dret = sum(dmodel_deta(tdata,cdata,theta,eta))
     #dret=c3*eta-dret
     return [ret, dret]
-

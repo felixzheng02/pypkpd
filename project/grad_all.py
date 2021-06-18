84a0d175f5a941591cfab5a6d66919604bffe4ba
Caiya-Zhang <69797470+Caiya-Zhang@users.noreply.github.com>
2021-06-18 20:23:24 +0800

    continue hessian*

--- project/grad_all.py
+++ project/grad_all.py
@@ -5,9 +5,9 @@ Author: Caiya Zhang, Yuchen Zheng
 """
 ##!!!!f
 
-from numpy import ndarray
+import numpy as np
 from project.zeros import zeros
-
+from project.size import size
 from project.feval import do_call
 
 def grad_all (func, select_par, nRow, *args, subset=None, currentOcc=None, noPopED=False, offdiag=False):
@@ -20,23 +20,23 @@ def grad_all (func, select_par, nRow, *args, subset=None, currentOcc=None, noPop
     
     if currentOcc is None:
 
-        idx = idx0 = seq_along(def0)
+        idx = idx0 = len(def0)
         if subset != None:
             idx0 = as.vector(cumsum(subset)*subset)
             idx = seq(max(idx0))
     
-        if def0 is ndarray and select_par > 6 and offdiag == False:
-            idx0 = diag(idx0)
+        if type(def0) == np.ndarray and select_par > 6 and offdiag == False:
+            idx0 = np.diag(idx0)
     
-        if is.matrix(def0) and select_par>6 and offdiag == True:
-            tmp = 0*def0
+        if type(def0) == np.ndarray and select_par > 6 and offdiag == True:
+            tmp = 0 * def0
             tmp[lower.tri(tmp)] = idx0
-            idx0 = tmp + t(tmp)
+            idx0 = tmp + np.transpose(tmp)
     
     else:
         idx  = seq(size(def0,1))
         idx0 = zeros(size(def0))
-        idx0[,currentOcc] = idx
+        idx0[:, currentOcc] = idx
   
 
     poped_db = arg_list[[len(arg_list)]]
@@ -46,19 +46,19 @@ def grad_all (func, select_par, nRow, *args, subset=None, currentOcc=None, noPop
     if noPopED == True: 
       arg_list = arg_list[-len(arg_list)]
   
-    gradX = zeros(nRow, length(idx))
+    gradX = zeros(nRow, len(idx))
 
     #Central approximation
     if grad_all_switch == 1:
         for i in idx:
-            arg_list[[select_par]] = def0 + (idx0 == i)*hlf
+            arg_list[[select_par]] = def0 + (idx0 == i) * hlf
             def_plus <- do_call(func, arg_list)
-            arg_list[[select_par]] = def0 - (idx0 == i)*hlf
+            arg_list[[select_par]] = def0 - (idx0 == i) * hlf
             def_minus <- do_call(func, arg_list)
             if noPopED == False:
                 def_plus = def_plus[[1]]        
                 def_minus = def_minus[[1]]        
-            gradX[,i] = (def_plus - def_minus)/(2.0*hlf)
+            gradX[:,i] = (def_plus - def_minus) / (2.0 * hlf)
     
     else:
     #Complex approximation
@@ -66,7 +66,8 @@ def grad_all (func, select_par, nRow, *args, subset=None, currentOcc=None, noPop
             for i in idx:
                 arg_list[[select_par]] = def0 + (idx0 == i) * complex(real = 0, imaginary = hlf)
                 def_plus = do_call(func, arg_list)[[1]]
-                gradX[,i] = Im(def_plus) / hlf
+##没写！！！！pixel image
+                gradX[:,i] = Im(def_plus) / hlf
         else:
             raise Exception("Unknown derivative option for grad_all")
 

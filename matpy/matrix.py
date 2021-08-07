71c2f415033df9203891c1838d96073b151f34f6
felixzheng02 <felix02@email.unc.edu>
2021-08-07 16:33:01 +0800

    num class

---
+++ matpy/matrix.py
@@ -0,0 +1,62 @@
+"""
+matpy is a self-defined class that allows users to 
+name rows, columns, and elements of matrices
+
+Authors: Caiya Zhang, Yuchen Zheng
+"""
+
+
+import numpy as np
+import pandas as pd
+
+
+class matrix:
+
+	def __init__(self, data: np.ndarray, shape: tuple, datanam: list = None, colnam: list = None, rownam: list = None):
+		# data field:
+		# data
+		# shape
+		# datanam
+		# colnam
+		# rownam
+		self.data = data.reshape(shape)
+		self.shape = shape
+		self.datanam = datanam
+		self.colnam = colnam
+		self.rownam = rownam
+	
+	def get_data(self):
+		return self.data
+
+	def get_shape(self):
+		return self.shape
+
+	def get_datanam(self):
+		return self.datanam
+
+	def get_colnam(self):
+		return self.colnam
+
+	def get_rownam(self):
+		return self.rownam
+
+	def get_one_data(self, name: str):
+		if self.get_datanam() is not None:
+			for index in range(0, len(self.get_datanam())):
+				if name == self.get_datanam()[index]:
+					return self.get_data.flatten()[index]
+		raise Exception("'%s' does not exists." % name)
+
+	def set_datanam(self, datanam: list):
+		self.datanam = datanam
+
+	def set_colnam(self, colnam: list):
+		self.colnam = colnam
+
+	def set_rownam(self, rownam: list):
+		self.rownam = rownam
+
+	def create_dataframe(self):
+		return pd.DataFrame(self.get_data(),
+							columns=self.get_colnam(),
+							index=self.get_rownam())
---
+++ matpy/num.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class num:
+
+	def __init__(self, input):
+		# data field:
+		# value
+		# type
+		if type(input) is list:
+			self.value = np.array(input)
+		else: # int or np.ndarray
+			self.value = input
+		self.type = type(self.value)
+	
+	def get_value(self):
+		return self.value
+
+	def trans_array(self):
+		"""return self.value as an np.ndarray"""
+		return np.array(self.get_value)
+
+	def get_by_index(self, index: int): # only 1-d array is considered
+		"""return value by index, nan if out of index"""
+		index_max = np.size(self.trans_array())
+		if index < index_max:
+			return self.trans_array()[index]
+		else:
+			return np.nan

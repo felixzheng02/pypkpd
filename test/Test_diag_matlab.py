d290ec120d25640503faf93efa492cc2c37d6ed2
Caiya Zhang <69797470+Caiya-Zhang@users.noreply.github.com>
2021-08-08 09:02:51 -0400

    update test cases for matrix type input

---
+++ test/Test_diag_matlab.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+from matpy.matrix import matrix
+from project.diag_matlab import diag_matlab
+
+
+a = matrix(np.array([1,2,3]))
+b = matrix(np.array([1,2,3,4,5,6,7,8,9]),(3,3))
+print(diag_matlab(a).get_data())
+print(diag_matlab(b).get_data())
--- test/Test_size.py
+++ test/Test_size.py
@@ -1,9 +1,10 @@
 import numpy as np
-from project.zeros import zeros
 from project.size import size
+from project.zeros import zeros
+from matpy.matrix import matrix
 
 
-l1 = size(obj=np.array([2,3,4,5,6]))
+l1 = size(obj=matrix(np.array([2,3,4,5,6])))
 
 l2 = size(10)
 

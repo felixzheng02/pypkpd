4244b284ed6f49f81dd1c8432b8c90da522fe341
Caiya Zhang <69797470+Caiya-Zhang@users.noreply.github.com>
2021-08-16 14:06:05 -0400

    Add files via upload

--- test/Test_diag_matlab.py
+++ test/Test_diag_matlab.py
@@ -6,5 +6,5 @@ from project.diag_matlab import diag_matlab
 
 a = matrix(np.array([1,2,3]))
 b = matrix(np.array([1,2,3,4,5,6,7,8,9]),(3,3))
-print(diag_matlab(a).get_data())
-print(diag_matlab(b).get_data())
+print(diag_matlab(a).get_all_data())
+print(diag_matlab(b).get_all_data())

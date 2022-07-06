9a5ca43e6501d91aa6123f0655defce4409d5ba5
Felix Zheng <felix02@email.unc.edu>
2022-07-06 16:43:26 -0400

    update Test_create_design_space

--- test/Test_create_design_space.py
+++ test/Test_create_design_space.py
@@ -25,68 +25,62 @@ class TestCreateDesign(unittest.TestCase):
 
         ds_5 = create_design_space(design_1, mina=Matrix([10,20]))
 
-        # design_2 = create_design(xt=Matrix([[1,2,3,4,5], [1,2,3,4]]),
-        #                  groupsize=Matrix([50, 20]),
-        #                  a=Matrix([[70, 1000],[35, 1000]], axisnam=[None, ["WT", "DOSE"]]),
-        #                  x=Matrix([[1, 100], [2, 200]], axisnam=[None, ["SEX", "DOSE_discrete"]]))
+        design_2 = create_design(xt=Matrix([[1,2,3,4,5], [1,2,3,4]]),
+                         groupsize=Matrix([50, 20]),
+                         a=Matrix([[70, 1000],[35, 1000]], axisnam=[None, ["WT", "DOSE"]]),
+                         x=Matrix([[1, 100], [2, 200]], axisnam=[None, ["SEX", "DOSE_discrete"]]))
 
-        # ds_6 = create_design_space(design_2) 
+        ds_6 = create_design_space(design_2) 
 
-        # ds_7 = create_design_space(design_2,
-        #                         x_space=[np.array([1,2]),
-        #                                     np.array(list(range(100,420,20)))])
+        ds_7 = create_design_space(design_2,
+                                x_space=Matrix([[1, 2], list(range(100, 420, 20))], 
+                                axisnam=[["SEX", "DOSE_discrete"], None]))
 
-        # ds_8 = create_design_space(design_2,
-        #                         x_space=[np.array([1,2]),
-        #                                     np.array(list(range(100,420,20)))],
-        #                         grouped_xt=np.array([1,2,3,4,5])) 
+        ds_8 = create_design_space(design_2,
+                                x_space=Matrix([[1, 2], list(range(100, 420, 20))], 
+                                axisnam=[["SEX", "DOSE_discrete"], None]),
+                                grouped_xt=np.array([1, 2, 3, 4, 5]))
 
-        # ds_9 = create_design_space(design_2,
-        #                         x_space=[np.array([1,2]),
-        #                                     np.array(list(range(100,420,20)))],
-        #                         use_grouped_xt=True) 
+        ds_9 = create_design_space(design_2,
+                                x_space=Matrix([[1, 2], list(range(100, 420, 20))], 
+                                axisnam=[["SEX", "DOSE_discrete"], None]),
+                                use_grouped_xt=True)
+                               
+        design_3 = create_design(xt=Matrix([[1,2,3,4,5], [1,2,3,4]]),
+                         groupsize=Matrix([50, 20]),
+                         a=Matrix([35, 1000], axisnam=[None, ["WT", "DOSE"]]),
+                         x=Matrix([1, 100], axisnam=[None, ["SEX", "DOSE_discrete"]]))
+
+        ds_10 = create_design_space(design_3,
+                                x_space=Matrix([[1, 2], list(range(100, 420, 20))], 
+                                axisnam=[["SEX", "DOSE_discrete"], None]),
+                                use_grouped_a=True)
+
+        ds_11 = create_design_space(design_2,
+                                x_space=Matrix([[1, 2], list(range(100, 420, 20))], 
+                                axisnam=[["SEX", "DOSE_discrete"], None]),
+                                grouped_a=np.array([[1, 2], [3, 2]]))
+
+        ds_12 = create_design_space(design_3,
+                                x_space=Matrix([[1, 2], list(range(100, 420, 20))], 
+                                axisnam=[["SEX", "DOSE_discrete"], None]),
+                                use_grouped_xt=True)
+
+        ds_13 = create_design_space(design_3,
+                                x_space=Matrix([[1, 2], list(range(100, 420, 20))], 
+                                axisnam=[["SEX", "DOSE_discrete"], None]),
+                                grouped_x=np.array([1, 2], [3, 2]))
+
+        # seq_1 = 1:10
+        seq_1 = list(range(1, 11))
+        
+        ds_14 = create_design_space(design_1,maxxt=10,minxt=0,
+                                    xt_space = [seq_1,seq_1,seq_1,seq_1,seq_1])
+        ds_15 = create_design_space(design_1,maxxt=10,minxt=0,xt_space = [seq_1])
+
+        # ds_16 = create_design_space(design_1,maxxt=10,minxt=0,xt_space = xt_space)
+
+        ds_17 = create_design_space(design_1,a_space=[list(range(1, 100)), list(range(1000, 101000, 1000))])
+
 if __name__ == '__main__':
     unittest.main()
-
-
-# design_3 = create_design(xt=[np.array([1,2,3,4,5]),
-#                              np.array([1,2,3,4])],
-#                          groupsize=np.array([50,20]),
-#                          a = [np.array([35, 1000])],
-#                          x = [np.array([1, 100])]
-# )
-
-# ds_10 = create_design_space(design_3,
-#                            x_space=[np.array([1,2]),
-#                                     np.array(list(range(100,420,20)))],
-#                            use_grouped_a=True)
-
-# ds_11 = create_design_space(design_2,
-#                            x_space=[np.array([1,2]),
-#                                     np.array(list(range(100,420,20)))],
-#                            grouped_a=[np.array([1,2]), np.array([3,2])])
-
-# ds_12 = create_design_space(design_3,
-#                            x_space=[np.array([1,2]),
-#                                     np.array(list(range(100,420,20)))],
-#                            use_grouped_x=True)
-
-# ds_13 = create_design_space(design_3,
-#                            x_space=[np.array([1,2]),
-#                                     np.array(list(range(100,420,20)))],
-#                            grouped_x=[np.array([1,2]), np.array([3,2])])
-
-
-# # seq_1 = 1:10
-# seq_1 = list(range(1, 11))
-# ds_14 = create_design_space(design_1,maxxt=10,minxt=0,
-#                              xt_space = [seq_1,seq_1,seq_1,seq_1,seq_1])
-# ds_15 = create_design_space(design_1,maxxt=10,minxt=0,xt_space = [seq_1])
-
-# # possible_values = as.Matrix(cbind(list(0:10),list(0:10),list(0:10),list(0:20),list(0:20)))
-# # xt_space = as.Matrix(rbind(possible_values,possible_values))
-
-# # ds_16 = create_design_space(design_1,maxxt=10,minxt=0,xt_space = xt_space)
-
-# # ds_17 = create_design_space(design_1,a_space = list(1:100,seq(1000,100000,by=1000)))
-

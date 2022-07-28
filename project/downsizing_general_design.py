1af404bb25a733df04eb4cbd11197c6cfa2abae8
Felix Zheng <felix02@email.unc.edu>
2022-07-28 18:50:16 -0400

    finished downsizing_general_design

--- project/downsizing_general_design.py
+++ project/downsizing_general_design.py
@@ -26,40 +26,40 @@ import numpy as np
 from project.size import size
 from project.util import get_dict_value
 from project.zeros import zeros
+from matpy.matrix import Matrix
 
 def downsizing_general_design(pypkpd_db):
     # ------------- downsizing of general design
     
     m = get_dict_value(pypkpd_db, "design", "m").get_value()
     ni = get_dict_value(pypkpd_db, "design", "ni").get_partial_matrix([[0, m],[None, None]])
-    xt = pypkpd_db["design"]["xt"].get_partial_matrix([[0, m],[0, np.max(get_dict_value(pypkpd_db, "design_space", "maxni").get_data())]])
-    xt = pypkpd_db["design"]["xt"][1:pypkpd_db["design"]["m"],1:max(pypkpd_db["design_space"]["maxni"])]
-    model_switch = pypkpd_db["design"]["model_switch"][0:pypkpd_db["design"]["m"],0:max(pypkpd_db["design_space"]["maxni"])]
+    xt = get_dict_value(pypkpd_db, "design", "xt").get_partial_matrix([[0, m],[0, np.max(get_dict_value(pypkpd_db, "design_space", "maxni").get_data())]])
+    model_switch = get_dict_value(pypkpd_db, "design", "model_switch").get_partial_matrix([[0, m],[0, np.max(get_dict_value(pypkpd_db, "design_space", "maxni").get_data())]])
     
-    if size(pypkpd_db["design"]["x"])[1] != 0:
-        x = pypkpd_db["design"]["x"][1:pypkpd_db["design"]["m"],0:size(pypkpd_db["design"]["x"])[1]]
+    if get_dict_value(pypkpd_db, "design", "x").get_shape(1) != 0:
+        x = get_dict_value(pypkpd_db, "design", "x").get_partial_matrix([[0, m], [0, None]])
     else:
-        x = zeros(pypkpd_db["design"]["m"], 0)
+        x = Matrix(np.zeros([m, 0])) # different from R: np.zeros([1, 0]) -> array([], shape=(1, 0), dtype=float64)
     
-    if size(pypkpd_db["design"]["a"])[1] != 0:
-        a = pypkpd_db["design"]["a"][0:pypkpd_db["design"]["m"],0:size(pypkpd_db["design"]["a"])[1]]
-        maxa = pypkpd_db["design_space"]["maxa"][0:pypkpd_db["design"]["m"],0:size(pypkpd_db["design"]["a"])[1]]
-        mina = pypkpd_db["design_space"]["mina"][0:pypkpd_db["design"]["m"],0:size(pypkpd_db["design"]["a"])[1]]
+    if get_dict_value(pypkpd_db, "design", "a").get_shape(1) != 0:
+        a = get_dict_value(pypkpd_db, "design", "a").get_partial_matrix([[0, m], [0, None]])
+        maxa = get_dict_value(pypkpd_db, "design", "maxa").get_partial_matrix([[0, m], [0, get_dict_value(pypkpd_db, "design", "a").get_shape(1)]])
+        mina = get_dict_value(pypkpd_db, "design", "mina").get_partial_matrix([[0, m], [0, get_dict_value(pypkpd_db, "design", "a").get_shape(1)]])
     else:
-        a = zeros(pypkpd_db["design"]["m"],0)
-        maxa = np.zeros(1)
-        mina = np.zeros(1)
+        a = Matrix(np.zeros(m, 0))
+        maxa = Matrix(np.zeros([0, 0])) # different from R
+        mina = Matrix(np.zeros([0, 0]))
     
-    bpop = pypkpd_db["parameters"]["bpop"][0:pypkpd_db["parameters"]["nbpop"],0:2]
-    n = np.matmul(np.transpose(ni),np.ones(pypkpd_db["design"]["m"],1))
+    bpop = get_dict_value(pypkpd_db, "parameters", "bpop").get_partial_matrix([[0, get_dict_value(pypkpd_db, "parameters", "nbpop")], [0, 3]])
+    n = Matrix(np.matmul(np.transpose(ni.get_data()), np.ones(m, 1)))
     
-    if pypkpd_db["parameters"]["NumRanEff"] != 0:
-        d=pypkpd_db["parameters"]["d"][0:pypkpd_db["parameters"]["NumRanEff"],0:2]
+    if get_dict_value(pypkpd_db, "parameters", "NumRanEff") != 0:
+        d = get_dict_value(pypkpd_db, "parameters", "d").get_partial_matrix([[0, get_dict_value(pypkpd_db, "parameters", "NumRanEff")], [0, 3]])
     else:
-        d=pypkpd_db["parameters"]["d"]
+        d = get_dict_value(pypkpd_db, "parameters", "d")
     
-    maxxt=pypkpd_db["design_space"]["maxxt"][0:pypkpd_db["design"]["m"],0:max(pypkpd_db["design_space"]["maxni"])]
-    minxt=pypkpd_db["design_space"]["minxt"][0:pypkpd_db["design"]["m"],0:max(pypkpd_db["design_space"]["maxni"])]
+    maxxt = get_dict_value(pypkpd_db, "design_space", "maxxt").get_partial_matrix([[0, m], [0, np.max(get_dict_value(pypkpd_db, "design_space", "maxni").get_data())]])
+    minxt = get_dict_value(pypkpd_db, "design_space", "minxt").get_partial_matrix([[0, m], [0, np.max(get_dict_value(pypkpd_db, "design_space", "maxni").get_data())]])
     
     return {"ni": ni, "xt": xt, "model_switch": model_switch, "x": x, "a": a, "bpop": bpop, 
                 "n": n, "d": d, "maxxt": maxxt, "minxt": minxt, "maxa": maxa, "mina": mina}
